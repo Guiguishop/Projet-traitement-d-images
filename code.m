14d0c165b5e01ebc61c8bfe6e48148a4bc0112bd
GUILLOMEAU Nathan <Nathan.Guillomeau@bordeaux-inp.fr>
2021-11-25 12:07:02 +0100

    Ajout image et maj code

--- code.m
+++ code.m
@@ -3,3 +3,29 @@ clear all
 close all
 
 %%
+
+img1 = imread('background.jpg');
+img2 = imread('foreground.jpg');
+%figure, imshow(img1);
+%[X1,Y1] = ginput(4);
+%close
+figure,imshow(img2);
+[X2,Y2] = ginput(4);
+close
+
+%%
+
+H = zeros(3,3);
+H(3,3)=1;
+
+B = zeros(8,1);
+
+if (length(X2) == length(Y2))
+    for i=1:length(X2)
+        B(2*(i-1)+1,1)=X2(i);
+        B(2*(i-1)+2,1)=Y2(i);
+    end
+else
+    disp("Error dim");
+end
+
