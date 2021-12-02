fe143e30171f0f278bc2d8aed86a3c3caa9405f5
GUILLOMEAU Nathan <Nathan.Guillomeau@bordeaux-inp.fr>
2021-12-02 10:21:43 +0100

    merge

--- code.m
+++ code.m
@@ -2,14 +2,16 @@
 %clear all
 close all
 
-%% Images entrées
+%% Images entrï¿½es
 
 img1 = imread('background.jpg');
 img2 = imread('foreground.jpg');
 figure, imshow(img1);
 [X1,Y1] = ginput(4);
+close;
 figure,imshow(img2);
 [X2,Y2] = ginput(4);
+close;
 
 
 %% Calcul de H
@@ -51,9 +53,19 @@ for i=1:8
             if(j==8 && mod(i,2)~=0)
                 A(i,j) = -Y1(index)*X2(index);
             end
-            disp(["index vaut" index])
+            
     end
 end
 
 H_=A\B;
 H=[H_(1) H_(2) H_(3);H_(4) H_(5) H_(6);H_(7) H_(8) 1];
+%%
+[h,w,z] = size(img);
+
+
+for i=1:w
+    for j=1:h
+        
+    end
+end
+
