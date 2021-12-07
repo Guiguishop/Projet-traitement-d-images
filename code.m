88f4275e7541b341739486a3842054f373429301
Guiguishop <71508215+Guiguishop@users.noreply.github.com>
2021-12-07 13:56:07 +0100

    Bug homographie marche pas encore

--- code.m
+++ code.m
@@ -5,12 +5,10 @@ close all
 %% Images entr�es
 img1 = imread('rectanglebleu.jpg');
 img2 = imread('panneau.jpg');
-figure, imshow(img1);
-[X1,Y1] = ginput(4);
-close;
-figure,imshow(img2);
-[X2,Y2] = ginput(4);
-close;
+% figure, imshow(img1);
+% [X1,Y1] = ginput(4);
+% figure,imshow(img2);
+%  [X2,Y2] = ginput(4);
 
 
 %%
@@ -63,28 +61,38 @@ H=homographie(X1,X2,Y1,Y2);
 
 %% On applique H au pixels de l'image que l'on veut retoucher (checker si on est dans le rectangle)
 
-
+[h,w,z] = size(img1);
 [h2,w2,z2] = size(img2);
 M2 = zeros(3,1);
 M= zeros(3,1);
-%%
-for i=1:w2
-    for j=1:h2
-        x2= H(1,1)*X1
-        M = H*[i;j;1];
-        x= M(1);
-        y = M(2);
 
-        if (x>=X1(1) && x<=X1(4) && y>=Y1(1) && y<=Y1(4)) % On vérifie si on est dans le rectangle
-            M2= inv(H)*M;
-            x2= M2(1);
-            y2= M(2);
-            img2(round(x2),round(y2),:)=img1(round(x1),round(x2),:);
+% for i=1:w2
+%     for j=1:h2
+%         x2= H(1,1)*X1
+%         M = H*[i;j;1];
+%         x= M(1);
+%         y = M(2);
+% 
+%         if (x>=X1(1) && x<=X1(4) && y>=Y1(1) && y<=Y1(4)) % On vérifie si on est dans le rectangle
+%             M2= inv(H)*M;
+%             x2= M2(1);
+%             y2= M(2);
+%             img2(round(x2),round(y2),:)=img1(round(x1),round(x2),:);
+%         end
+%        
+%     end
+% end
+for i=1:w
+    for j=1:h
+        x2= (H(1,1)*i + H(1,2)*j +H(1,3))/(H(3,1)*i+H(3,2)*j +H(3,3));
+        y2= (H(2,1)*i + H(2,2)*j +H(2,3))/(H(3,1)*i + H(3,2)*j+H(3,3));
+        if(x2>=1 && x2<=w && y2>=1 && y2<=h)
+            img2(j,i,:)=img1(floor(y2),floor(x2),:);
         end
-       
     end
 end
-imshow(img2);
+            
+figure,imshow(img2);
 %Utiliser triplet(image,masque,boite englobante) masque on initialise avec que des 1
 %Calculer des nouvelles boites englobantes en appliquant l'homographie �
 %l'image de base (aux 4 points) puis min et max de ces 4 nouveaux points
